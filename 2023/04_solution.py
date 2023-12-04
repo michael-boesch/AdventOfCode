3f297e6a48655a16933472d99af2185bd9d69fe9
Michael Bösch <michael.boesch@mailbox.org>
2023-12-04 20:25:22 +0100

    added part 2 of day 4

--- 2023/04_solution.py
+++ 2023/04_solution.py
@@ -21,4 +21,25 @@ for card in cards:
     if sum_card > 0:
         sum_deck += 2**(sum_card-1)
         
+print(sum_deck)
+        
 # part 2
+
+counters = [1]*len(cards)
+
+for i, card in enumerate(cards):
+    winning_numbers = [int(item) for item in card.split('|')[0].split(':')[1].split()]
+    my_numbers = [int(item) for item in card.split('|')[1].split()]
+
+    sum_card = 0
+    for my_number in my_numbers:
+        for winning_number in winning_numbers:
+            if my_number == winning_number:
+                sum_card += 1
+
+    if sum_card > 0:
+        for j in range(i+1, i+sum_card+1):
+            if j < len(counters):
+                counters[j] += counters[i]
+
+print(sum(counters))
